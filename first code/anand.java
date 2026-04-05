e71cc9d228b472a6e7129edd1cadbe602f9aa033
anand <yadavanandyadav38@gmail.com>
2026-04-05 23:34:21 +0530

    Inverted Half pyramid

--- first code/anand.java
+++ first code/anand.java
@@ -1,20 +1,15 @@
 public class anand {
     public static void main(String[] args) {
-       int n = 4; 
-       // outer loop
-       for(int i = 1; i <= n; i++) {
+       int n = 4;
 
-        //inner loop -> spaces print
-        for(int j = 1; j <= n; j++) {
-            System.out.print(" ");
-        }
-        // inner loop -> star print
+       //outer loop
+       for(int i = n; i >= 1; i--) {
+        // inner loop
         for(int j = 1; j <= i; j++) {
             System.out.print("*");
         }
         System.out.println();
-
-       }
+        }
 
         
     }
