a0c93b8b84b2236a61484b272d0d96bf6e13cb7f
anand <yadavanandyadav38@gmail.com>
2026-04-11 00:38:48 +0530

    name in a function

--- first code/anand.java
+++ first code/anand.java
@@ -1,15 +1,15 @@
 import java.util.*;
 public class anand {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int sum = calculateSum(a, b);
-        System.out.println(sum);
+        anandMethod();
     }
-
-    public static int calculateSum(int a, int b) {
-        int sum = a + b;
-        return sum;
+    public static void anandMethod() {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        printMyName(name);
+    }
+    
+    public static void printMyName(String name) {
+        System.out.println("My name is: " + name);
     }
 }
